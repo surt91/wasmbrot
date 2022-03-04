3f67d273f6414ebf2966c8f8c55ce93614a5a231
Hendrik Schawe <hendrik.schawe@codecentric.de>
2022-03-04 14:09:21 +0100

    a mandelbrot is drawn

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 mod utils;
 
+use num::complex::Complex;
+
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -9,11 +11,85 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+pub struct MandelbrotCanvas {
+    width: u32,
+    height: u32,
+    // zoom: f64,
+    escape_times: Vec<u8>,
+    pixels: Vec<u8>,
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {name}!"));
+impl MandelbrotCanvas {
+    pub fn new(width: u32, height: u32) -> MandelbrotCanvas {
+        // TODO: init escape times
+        MandelbrotCanvas {
+            width,
+            height,
+            // zoom: 1./width as f64,
+            escape_times: vec![0; (width*height) as usize],
+            pixels: vec![0; (4*width*height) as usize]
+        }
+    }
+
+    pub fn mandelbrot(&mut self) {
+        let zoom = 1./self.width as f64;
+        let x = -1.;
+        let y = 0.;
+
+        for j in 0..self.height {
+            for i in 0..self.width {
+                let idx = (j*self.width + i) as usize;
+
+                let xp = (i as f64 - self.width as f64 / 2.) as f64 * zoom + x;
+                let yp = (j as f64 - self.height as f64 / 2.) as f64 * zoom + y;
+                let p = Complex::new(xp, yp);
+
+                self.escape_times[idx] = MandelbrotCanvas::time_to_diverge(p);
+            }
+        }
+
+        self.update_pixels();
+    }
+
+    pub fn pixels(&self) -> *const u8 {
+        self.pixels.as_ptr()
+    }
+
+    fn update_pixels(&mut self) {
+        for j in 0..self.height {
+            for i in 0..self.width {
+                let idx = (j*self.width + i) as usize;
+
+                self.pixels[4*idx] = 255 - self.escape_times[idx];
+                self.pixels[4*idx+1] = 255 - self.escape_times[idx];
+                self.pixels[4*idx+2] = 255 - self.escape_times[idx];
+                self.pixels[4*idx+3] = 255;
+                // self.pixels[4*idx] = 200;
+                // self.pixels[4*idx+1] = 200;
+                // self.pixels[4*idx+2] = 200;
+                // self.pixels[4*idx+3] = 200;
+            }
+        }
+    }
+
+    fn time_to_diverge(mut state: Complex<f64>) -> u8 {
+        // threshold is 2^2, since we compare to the square of the norm
+        // as soon as the norm is >= 2 it is sure to diverge
+        let threshold = 4.;
+
+        // abort after 255 iterations
+        let max_count = 255;
+
+        let c = state;
+
+        let mut ctr = 0;
+        while {
+            state = state * state + c;
+            ctr += 1;
+
+            state.norm_sqr() < threshold && ctr < max_count
+        } {}
+        ctr
+    }
 }
--- www/index.js
+++ www/index.js
@@ -1,3 +1,18 @@
-import * as wasm from "wasmbrot";
+import {MandelbrotCanvas} from "wasmbrot";
+import { memory } from "wasmbrot/wasmbrot_bg";
 
-wasm.greet("Hendrik");
+const width = 1000;
+const height = 1000;
+
+let mandelbrot = MandelbrotCanvas.new(width, height)
+mandelbrot.mandelbrot();
+
+const canvas = document.getElementById("mandelbrot-canvas");
+const ctx = canvas.getContext('2d');
+
+const escapeTimesPtr = mandelbrot.pixels();
+const escapeTimes = new Uint8ClampedArray(memory.buffer, escapeTimesPtr, 4 * width * height);
+console.log(escapeTimes);
+const image = new ImageData(escapeTimes, width, height);
+console.log(image);
+ctx.putImageData(image, 0, 0);
